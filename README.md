f1c4ebf1f645e42a027e1fd15d214aca9cdc8e23
Guilherme Vieira <super.driver.512@gmail.com>
2013-07-15 05:42:54 -0300

    Initial commit.
    src/xdy.js will be rewritten under a TDD regime before v0.0.1.

---
+++ README.md
@@ -0,0 +1,4 @@
+xdy [![Build Status](https://travis-ci.org/n2liquid/xdy.png?branch=master)](https://travis-ci.org/n2liquid/xdy)
+===
+
+Dynamically setup / remove prototype chain mixins.
---
+++ src/xdy.js
@@ -0,0 +1,61 @@
+'use strict';
+
+function mixin (target, object, property)
+{
+	if (object[property] === undefined)
+	{
+		throw new ReferenceError('Property "' + property + '" is undefined.');
+	}
+
+	if (typeof object[property] === 'function')
+	{
+		target[property] = object[property].bind(object);
+	}
+	else
+	{
+		Object.defineProperty
+		(
+			target, property,
+			{
+				get: function () { return object[property]; },
+				set: function (value) { return object[property] = value; }
+			}
+		);
+	}
+}
+
+function make_node (object)
+{
+	remove_node(object);
+
+	var mixin_node = { __mixin_node: true };
+
+	mixin_node.__proto__ = object.__proto__;
+	object.__proto__ = mixin_node;
+
+	return mixin_node;
+}
+
+function get_node (object)
+{
+	if (!object.__proto__.__mixin_node)
+	{
+		return null;
+	}
+
+	return object.__proto__;
+}
+
+function remove_node (object)
+{
+	if (object.__proto__.__mixin_node)
+	{
+		object.__proto__ = object.__proto__.__proto__;
+	}
+}
+
+mixin.make_node = make_node;
+mixin.get_node = get_node;
+mixin.remove_node = remove_node;
+
+module.exports = mixin;
